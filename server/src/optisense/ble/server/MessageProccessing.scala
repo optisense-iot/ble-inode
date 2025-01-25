3860ac2aec674e904ba7035fdb4d177be5141728
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-01-25 17:38:37 +0100

    Capture mac address of the source device

--- server/src/optisense/ble/server/MessageProccessing.scala
+++ server/src/optisense/ble/server/MessageProccessing.scala
@@ -8,6 +8,8 @@ import optisense.ble.inode.INodeParser
 import optisense.ble.server.ServerApp.Config
 import scodec.bits.BitVector
 
+case class DeviceData[T](data: T, macAddress: String)
+
 object MessageProccessing {
   def processMessages: Stream[IO, Message] => Stream[IO, FlatSensorData] =
     _.evalMap(msg => IO.fromEither(Converter(msg)))
@@ -17,19 +19,23 @@ object MessageProccessing {
           case Left(_) => Stream.eval(putStrLn[IO]("Not an data item, skipping...")) *> Stream.empty
           case Right(td) =>
             Stream.eval(putStrLn[IO](s"Data item detected: $td")) *>
-              Stream.eval(IO(BitVector.fromValidHex(td.rawData)))
+              Stream.eval(IO(DeviceData(BitVector.fromValidHex(td.rawData), td.mac)))
         }
       }
-      .flatMap(rawData => Stream.evalSeq(IO.fromTry(BleFrame.framesCodec.decodeValue(rawData).toTry)))
+      .flatMap { deviceData =>
+        Stream
+          .evalSeq(IO.fromTry(BleFrame.framesCodec.decodeValue(deviceData.data).toTry))
+          .map(bleFrame => DeviceData(bleFrame, deviceData.macAddress))
+      }
       .evalMapFilter {
-        case BleFrame.Flags(bytes) =>
-          putStrLn[IO]("Flags frame, skipping...").as(none)
-        case BleFrame.ManufactureData(bytes) =>
-          putStrLn[IO]("ManufactureData detected. Decoding...") *> IO
+        case DeviceData(BleFrame.Flags(bytes), macAddress) =>
+          putStrLn[IO](s"Flags frame from $macAddress, skipping...").as(none)
+        case DeviceData(BleFrame.ManufactureData(bytes), macAddress) =>
+          putStrLn[IO](s"ManufactureData detected from $macAddress. Decoding...") *> IO
             .fromTry(INodeParser.codec.decodeValue(bytes).toTry)
-            .map(_.some)
-        case BleFrame.Unknown(bytes) =>
-          putStrLn[IO]("Unknown BLE frame, skipping...").as(none)
+            .map(inode => DeviceData(inode, macAddress).some)
+        case DeviceData(BleFrame.Unknown(bytes), macAddress) =>
+          putStrLn[IO](s"Unknown BLE frame from $macAddress, skipping...").as(none)
       }
       .map(FlatSensorData.from)
 }
--- server/src/optisense/ble/server/OutputModel.scala
+++ server/src/optisense/ble/server/OutputModel.scala
@@ -30,6 +30,7 @@ case class FlatSensorData(
     rtto: Boolean,
     lowBattery: Boolean,
     signature: ByteVector,
+    macAddress: String,
 ) {
   def withPosition(position: Acceleration): FlatSensorData =
     copy(
@@ -41,51 +42,55 @@ case class FlatSensorData(
 }
 
 object FlatSensorData {
-  private def base(inode: INode): String => FlatSensorData =
-    tpe =>
-      FlatSensorData(
-        `type` = tpe,
-        temperature = inode.data.temperature,
-        humidity = None,
-        pressure = None,
-        alarmMoveAccelerometer = inode.data.alarms.moveAccelerometer,
-        alarmLevelAccelerometer = inode.data.alarms.levelAccelerometer,
-        alarmLevelTemperature = inode.data.alarms.levelTemperature,
-        alarmLevelHumidity = inode.data.alarms.levelHumidity,
-        alarmContactChange = inode.data.alarms.contactChange,
-        alarmMoveStopped = inode.data.alarms.moveStopped,
-        alarmMoveGTimer = inode.data.alarms.moveGTimer,
-        alarmLevelAccelerometerChange = inode.data.alarms.levelAccelerometerChange,
-        alarmLevelMagnetChange = inode.data.alarms.levelMagnetChange,
-        alarmLevelMagnetTimer = inode.data.alarms.levelMagnetTimer,
-        positionMotion = None,
-        positionAccX = None,
-        positionAccY = None,
-        positionAccZ = None,
-        batteryInfoLevel = inode.data.battery.level,
-        batteryInfoVoltage = inode.data.battery.voltage,
-        time = inode.data.time,
-        rtto = inode.flags.rtto,
-        lowBattery = inode.flags.lowBattery,
-        signature = inode.data.signature,
-      )
+  private def base(deviceData: DeviceData[INode]): String => FlatSensorData = { tpe =>
+    val inode = deviceData.data
+    FlatSensorData(
+      `type` = tpe,
+      temperature = inode.data.temperature,
+      humidity = None,
+      pressure = None,
+      alarmMoveAccelerometer = inode.data.alarms.moveAccelerometer,
+      alarmLevelAccelerometer = inode.data.alarms.levelAccelerometer,
+      alarmLevelTemperature = inode.data.alarms.levelTemperature,
+      alarmLevelHumidity = inode.data.alarms.levelHumidity,
+      alarmContactChange = inode.data.alarms.contactChange,
+      alarmMoveStopped = inode.data.alarms.moveStopped,
+      alarmMoveGTimer = inode.data.alarms.moveGTimer,
+      alarmLevelAccelerometerChange = inode.data.alarms.levelAccelerometerChange,
+      alarmLevelMagnetChange = inode.data.alarms.levelMagnetChange,
+      alarmLevelMagnetTimer = inode.data.alarms.levelMagnetTimer,
+      positionMotion = None,
+      positionAccX = None,
+      positionAccY = None,
+      positionAccZ = None,
+      batteryInfoLevel = inode.data.battery.level,
+      batteryInfoVoltage = inode.data.battery.voltage,
+      time = inode.data.time,
+      rtto = inode.flags.rtto,
+      lowBattery = inode.flags.lowBattery,
+      signature = inode.data.signature,
+      macAddress = deviceData.macAddress,
+    )
+  }
 
-  def from(inode: INode): FlatSensorData =
+  def from(deviceData: DeviceData[INode]): FlatSensorData = {
+    val inode = deviceData.data
     inode match {
       case INode.CareSensor1(flags, data) =>
-        base(inode)("1").withPosition(data.position)
+        base(deviceData)("1").withPosition(data.position)
       case INode.CareSensor2(flags, data) =>
-        base(inode)("2").withPosition(data.position)
+        base(deviceData)("2").withPosition(data.position)
       case INode.CareSensor3(flags, data) =>
-        base(inode)("3").withPosition(data.position).copy(humidity = data.humidity.some)
+        base(deviceData)("3").withPosition(data.position).copy(humidity = data.humidity.some)
       case INode.CareSensorT(flags, data) =>
-        base(inode)("T")
+        base(deviceData)("T")
       case INode.CareSensorHT(flags, data) =>
-        base(inode)("HT").copy(humidity = data.humidity.some)
+        base(deviceData)("HT").copy(humidity = data.humidity.some)
       case INode.CareSensorPT(flags, data) =>
-        base(inode)("PT").copy(pressure = data.pressure.some)
+        base(deviceData)("PT").copy(pressure = data.pressure.some)
       case INode.CareSensorPHT(flags, data) =>
-        base(inode)("PHT").copy(pressure = data.pressure.some, humidity = data.humidity.some)
+        base(deviceData)("PHT").copy(pressure = data.pressure.some, humidity = data.humidity.some)
     }
+  }
 
 }
