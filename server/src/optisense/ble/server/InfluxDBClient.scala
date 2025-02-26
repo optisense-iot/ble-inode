96a8465694a3d56085e01e6fd77428eb9f298e87
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-02-26 17:50:00 +0100

    Use timestamp from parser instead of the one from ble

--- server/src/optisense/ble/server/InfluxDBClient.scala
+++ server/src/optisense/ble/server/InfluxDBClient.scala
@@ -1,5 +1,6 @@
 package optisense.ble.server
 
+import cats.effect.kernel.Clock
 import cats.effect.kernel.Resource
 import cats.effect.IO
 import sttp.client4._
@@ -13,54 +14,56 @@ class InfluxDBClient(backend: WebSocketBackend[IO], token: String) {
   val influxDBToken = "my-secret-token"
 
   def sendData(sensorData: FlatSensorData): IO[Response[Either[String, String]]] = {
-    val timestamp = sensorData.time
+    val dataF = Clock[IO].realTimeInstant.map(_.getNano()).map { timestamp =>
+      // InfluxDB Line Protocol
+      Seq(
+        Some(
+          s"""sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} temperature=${sensorData.temperature} $timestamp"""
+        ),
+        sensorData.humidity.map(h =>
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} humidity=$h $timestamp"
+        ),
+        sensorData.pressure.map(p =>
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} pressure=$p $timestamp"
+        ),
+        sensorData.positionMotion.map(m =>
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_motion=${if (m) 1 else 0} $timestamp"
+        ),
+        sensorData.positionAccX.map(x =>
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_acc_x=$x $timestamp"
+        ),
+        sensorData.positionAccY.map(y =>
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_acc_y=$y $timestamp"
+        ),
+        sensorData.positionAccZ.map(z =>
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_acc_z=$z $timestamp"
+        ),
+        Some(
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} battery_info_level=${sensorData.batteryInfoLevel} $timestamp"
+        ),
+        Some(
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} battery_info_voltage=${sensorData.batteryInfoVoltage} $timestamp"
+        ),
+        Some(s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} rtto=${if (sensorData.rtto) 1
+          else 0} $timestamp"),
+        Some(
+          s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} low_battery=${if (sensorData.lowBattery) 1
+            else 0} $timestamp"
+        ),
+      ).flatten.mkString("\n")
+    }
 
-    // InfluxDB Line Protocol
-    val data = Seq(
-      Some(
-        s"""sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} temperature=${sensorData.temperature} $timestamp"""
-      ),
-      sensorData.humidity.map(h =>
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} humidity=$h $timestamp"
-      ),
-      sensorData.pressure.map(p =>
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} pressure=$p $timestamp"
-      ),
-      sensorData.positionMotion.map(m =>
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_motion=${if (m) 1 else 0} $timestamp"
-      ),
-      sensorData.positionAccX.map(x =>
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_acc_x=$x $timestamp"
-      ),
-      sensorData.positionAccY.map(y =>
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_acc_y=$y $timestamp"
-      ),
-      sensorData.positionAccZ.map(z =>
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} position_acc_z=$z $timestamp"
-      ),
-      Some(
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} battery_info_level=${sensorData.batteryInfoLevel} $timestamp"
-      ),
-      Some(
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} battery_info_voltage=${sensorData.batteryInfoVoltage} $timestamp"
-      ),
-      Some(s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} rtto=${if (sensorData.rtto) 1
-        else 0} $timestamp"),
-      Some(
-        s"sensor_data,type=${sensorData.`type`},mac_address=${sensorData.macAddress} low_battery=${if (sensorData.lowBattery) 1
-          else 0} $timestamp"
-      ),
-    ).flatten.mkString("\n")
+    dataF.flatMap { data =>
+      val request = basicRequest
+        .post(influxDBUrl)
+        .header("Authorization", s"Token $influxDBToken")
+        .header("Content-Type", "text/plain; charset=utf-8")
+        .body(data)
 
-    val request = basicRequest
-      .post(influxDBUrl)
-      .header("Authorization", s"Token $influxDBToken")
-      .header("Content-Type", "text/plain; charset=utf-8")
-      .body(data)
-
-    IO.println(s"Sent to InfluxDB: $data") *>
-      request
-        .send(backend)
+      IO.println(s"Sent to InfluxDB: $data") *>
+        request
+          .send(backend)
+    }
   }
 }
 
